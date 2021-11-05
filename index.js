d6a4581bbc152db10054910748283913ba54b07f
franco fiorotto <fioroto.jf@gmail.com>
2021-11-04 23:56:45 -0300

    adding middleware to handle not found paths

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const routesV1 = require("./source/routes/v1");
+const { response } = require("express");
 
 const app = express();
 const port = 3001;
@@ -10,6 +11,12 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/v1", routesV1);
+
+app.use((req, res) => {
+	res.status(404).json({
+		error: "Not found",
+	});
+});
 
 app.listen(port, () => {
 	console.log(`App listening at http://localhost:${port}`);
